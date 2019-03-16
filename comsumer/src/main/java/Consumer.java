93e3323689c3a2d9c7da57aa4008a8fd94e807ad
ifdevil <384867771@qq.com>
2019-03-16 22:55:45 +0800

    consumer

--- comsumer/src/main/java/Consumer.java
+++ comsumer/src/main/java/Consumer.java
@@ -6,8 +6,8 @@ public class Consumer {
     public static void main(String[] args) {
         try {
             String name = "Service";
-            Registry registry = LocateRegistry.getRegistry(1002);
-            Service service = (Service) registry.lookup(name);
+            Registry registry = LocateRegistry.getRegistry(1002);//获取本地注册中心
+            Service service = (Service) registry.lookup(name);//寻找服务
             System.out.println(service.getUserName());
             System.out.println(service.getUser().getName());
         } catch (Exception e) {
